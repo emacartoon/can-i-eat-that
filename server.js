3a3336d6d4a04dc87657d77c1a61df7c88395216
EmaCartoon <emacartoon@gmail.com>
2021-05-10 09:39:38 -0700

    Removing MongoDB references

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 // const cors = require("cors");
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,8 +19,8 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
+// // Connect to the Mongo DB
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
 
 // Start the API server
 app.listen(PORT, function () {
